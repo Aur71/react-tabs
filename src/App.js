bf88411eb826055f342f6e36c753af1e8320fd0c
Aur71 <111057774+Aur71@users.noreply.github.com>
2022-10-14 11:54:05 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { FiChevronsRight } from 'react-icons/fi';
+
+const url = 'https://course-api.com/react-tabs-project';
+
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [index, setIndex] = useState(0);
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(url);
+      const data = await response.json();
+      setData(data);
+      setIndex(0);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <h1>Loading...</h1>
+      </main>
+    );
+  }
+
+  const { company, dates, duties, title } = data[index];
+  const companys = data.map((item) => {
+    return item.company;
+  });
+
+  return (
+    <main>
+      <header>
+        <h1>Experience</h1>
+        <div className="underline"></div>
+      </header>
+
+      <section>
+        <div className="btn-container">
+          {companys.map((item, indexx) => {
+            return (
+              <button
+                key={indexx}
+                onClick={() => setIndex(indexx)}
+                className={indexx === index && 'btn-active'}
+              >
+                {item}
+              </button>
+            );
+          })}
+        </div>
+
+        <div className="content">
+          <h2>{title}</h2>
+          <h3>{company}</h3>
+          <h4>{dates}</h4>
+          <ul>
+            {duties.map((item, index) => {
+              return (
+                <li key={index}>
+                  <FiChevronsRight /> {item}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default App;
